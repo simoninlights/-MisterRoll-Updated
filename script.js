e0a048d7b97b6b8caa19a99c189aa8599708410e
simoninlights <semion.ignatov@gmail.com>
2023-05-16 02:35:10 +0700

    added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /* Preloader */
-// document.addEventListener("DOMContentLoaded", function () {
-//     if (sessionStorage.getItem('dontLoad') === null) {
-//         setTimeout(function () {
-//             document.querySelector("body").classList.add("loaded");
-//         }, 0);
-//     } if (sessionStorage.getItem('dontLoad') === true) {
-//         document.addEventListener("DOMContentLoaded", function () {
-//             document.querySelector("body").classList.remove("loaded");
-//         });
-//     }
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  if (sessionStorage.getItem('dontLoad') === null) {
+    setTimeout(function () {
+      document.querySelector("body").classList.add("loaded");
+    }, 1000);
+  } if (sessionStorage.getItem('dontLoad') === true) {
+    document.addEventListener("DOMContentLoaded", function () {
+      document.querySelector("body").classList.remove("loaded");
+    });
+  }
+});
 
 
 /* -------Slider------- */
@@ -37,61 +37,61 @@ slides.style.transform = `translateX(${currentPosition}px)`;
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
 prevBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    movePrevious();
-  });
-  
-  nextBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    moveNext();
-  });
+  event.preventDefault();
+  movePrevious();
+});
+
+nextBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  moveNext();
+});
 
 function movePrevious() {
-    currentPosition += slideWidth;
-    slides.style.transition = "transform 0.5s ease-in-out";
-    slides.style.transform = `translateX(${currentPosition}px)`;
-  
-    // Handle edge case when current slide is first clone
-    if (currentPosition === 0) {
-      setTimeout(() => {
-        currentPosition = -slideWidth * (slides.children.length - 2);
-        slides.style.transition = "none";
-        slides.style.transform = `translateX(${currentPosition}px)`;
-      }, 500);
-    }
-  
-    if (currentPosition > 0) {
-      setTimeout(() => {
-        currentPosition = -slideWidth * (slides.children.length - 2);
-        slides.style.transition = "none";
-        slides.style.transform = `translateX(${currentPosition}px)`;
-      }, 500);
-    }
-  
-    // Reset transition property
+  currentPosition += slideWidth;
+  slides.style.transition = "transform 0.5s ease-in-out";
+  slides.style.transform = `translateX(${currentPosition}px)`;
+
+  // Handle edge case when current slide is first clone
+  if (currentPosition === 0) {
     setTimeout(() => {
-      slides.style.transition = "transform 0.5s ease-in-out";
-    }, 0);
+      currentPosition = -slideWidth * (slides.children.length - 2);
+      slides.style.transition = "none";
+      slides.style.transform = `translateX(${currentPosition}px)`;
+    }, 500);
   }
-  
-  function moveNext() {
-    currentPosition -= slideWidth;
-    slides.style.transition = "transform 0.5s ease-in-out";
-    slides.style.transform = `translateX(${currentPosition}px)`;
-  
-    if (currentPosition < -slideWidth * (slides.children.length - 2)) {
-      setTimeout(() => {
-        currentPosition = -slideWidth;
-        slides.style.transition = "none";
-        slides.style.transform = `translateX(${currentPosition}px)`;
-      }, 500);
-    }
-  
-    // Reset transition property
+
+  if (currentPosition > 0) {
     setTimeout(() => {
-      slides.style.transition = "transform 0.5s ease-in-out";
-    }, 0);
+      currentPosition = -slideWidth * (slides.children.length - 2);
+      slides.style.transition = "none";
+      slides.style.transform = `translateX(${currentPosition}px)`;
+    }, 500);
   }
+
+  // Reset transition property
+  setTimeout(() => {
+    slides.style.transition = "transform 0.5s ease-in-out";
+  }, 0);
+}
+
+function moveNext() {
+  currentPosition -= slideWidth;
+  slides.style.transition = "transform 0.5s ease-in-out";
+  slides.style.transform = `translateX(${currentPosition}px)`;
+
+  if (currentPosition < -slideWidth * (slides.children.length - 2)) {
+    setTimeout(() => {
+      currentPosition = -slideWidth;
+      slides.style.transition = "none";
+      slides.style.transform = `translateX(${currentPosition}px)`;
+    }, 500);
+  }
+
+  // Reset transition property
+  setTimeout(() => {
+    slides.style.transition = "transform 0.5s ease-in-out";
+  }, 0);
+}
 
 // Set interval to move to next slide every 4 seconds
 let slideInterval = setInterval(moveNext, 4000);
@@ -136,43 +136,43 @@ const modalBg = document.querySelector('.pop-container');
 const closeModal = document.querySelector('.pop-icon-cancel');
 
 modalBtn.addEventListener('click', () => {
-    modalBg.classList.add('pop-container-active');
-    console.log('add');
+  modalBg.classList.add('pop-container-active');
+  console.log('add');
 });
 
 closeModal.addEventListener('click', () => {
-    modalBg.classList.remove('pop-container-active');
-    console.log('remove');
+  modalBg.classList.remove('pop-container-active');
+  console.log('remove');
 });
 
 
 document.addEventListener('click', (e) => {
-    if (e.target === modalBg) {
-        modalBg.classList.remove('pop-container-active');
-    }
+  if (e.target === modalBg) {
+    modalBg.classList.remove('pop-container-active');
+  }
 });
 
 /* Scroll to top button */
 const scrollBtn = document.querySelector('.scroll-to-top');
 
 const refreshBtnVisibility = () => {
-    if (document.documentElement.scrollTop <= 150) {
-        scrollBtn.style.display = "none";
-    } else {
-        scrollBtn.style.display = "block";
-    }
+  if (document.documentElement.scrollTop <= 150) {
+    scrollBtn.style.display = "none";
+  } else {
+    scrollBtn.style.display = "block";
+  }
 };
 refreshBtnVisibility();
 window.scrollTo(0, 2000);
 scrollBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
 });
 
 document.addEventListener('scroll', (e) => {
-    refreshBtnVisibility();
+  refreshBtnVisibility();
 });
 
 
