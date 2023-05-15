fc0c154b6539e500bcd23d30c19a8eb32384751d
simoninlights <semion.ignatov@gmail.com>
2023-05-16 01:42:10 +0700

    added

--- script.js
+++ script.js
@@ -151,3 +151,32 @@ document.addEventListener('click', (e) => {
         modalBg.classList.remove('pop-container-active');
     }
 });
+
+/* Scroll to top button */
+const scrollBtn = document.querySelector('.scroll-to-top');
+
+const refreshBtnVisibility = () => {
+    if (document.documentElement.scrollTop <= 150) {
+        scrollBtn.style.display = "none";
+    } else {
+        scrollBtn.style.display = "block";
+    }
+};
+refreshBtnVisibility();
+window.scrollTo(0, 2000);
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+});
+
+document.addEventListener('scroll', (e) => {
+    refreshBtnVisibility();
+});
+
+
+//---------------DYNAMIC YEAR IN THE FOOTER----------------
+
+const year = new Date();
+document.querySelector('.current-year').innerHTML = year.getFullYear();
